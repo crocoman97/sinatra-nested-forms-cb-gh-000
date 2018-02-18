34f7f100cd16bdf6b9ff1d7905b4cd7936091dba
Rauof M <abdelrauof197@gmail.com>
2018-02-18 10:37:11 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,7 +4,19 @@ class Ship
   attr_reader :all 
   @@all = []
 
-
+  def initialize(attributes)
+    attributes.each { |k,v|
+      self.send("#{k}=",v)
+    }
+    @@all.<<(self)
+  end
   
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear
+    all.destroy_all
+  end
   
 end
