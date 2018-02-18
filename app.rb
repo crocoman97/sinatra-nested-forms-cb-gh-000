cd29b75baa08e6a3b19b21b8799a57f143b20859
Rauof M <abdelrauof197@gmail.com>
2018-02-18 11:54:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,8 +13,10 @@ module FormsLab
     end
     
     post '/pirates' do 
-      pirate = Pirate.new
-
+      pirate = Pirate.new(params["student"])
+      params["student"]["ships"].each { |attributes| 
+        Ship.new(attributes)
+      }
       @ships = Ship.all
       erb :'pirates/show'
     end
